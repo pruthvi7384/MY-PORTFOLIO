a5ed0ee97979187e7641d96020eaaca505717017
pruthvi7384 <pruthvirajrajput575@gmail.com>
2022-11-22 23:19:01 +0530

    Started Implementing Resume Component

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -3,14 +3,49 @@
     <!-- Navigation Bar Component Imported -->
     <navigation></navigation>
     <!--X- Navigation Bar Component Imported -X-->
-    <div class="container project mt-4">
+    <div class="container resume mt-4">
       <div class="row headdings">
         <h1>RESUME</h1>
         <p>Check My Resume</p>
       </div>
-      <div class="row">
-        <div class="col-xl-4"></div>
-        <div class="col-xl-8"></div>
+      <div class="row summary mt-3">
+        <div class="col-xl-12">
+          <h2>Summary</h2>
+          <div class="resume-iteam">
+            <p>{{ JSON.About }}</p>
+          </div>
+          <!-- <p>{{ JSON.About }}</p> -->
+        </div>
+      </div>
+      <div class="row background mt-3">
+        <div class="col-xl-6 p-3">
+          <h2>Education</h2>
+          <div
+            class="resume-iteam"
+            v-for="(data, index) in JSON.Education"
+            :key="index"
+          >
+            <h3 class="text-uppercase">
+              {{ data.title }} , {{ data.specialization }}
+            </h3>
+            <div class="box">
+              <p>
+                <span>{{ data.year }}</span>
+              </p>
+            </div>
+            <h4 class="text-uppercase">{{ data.univercity }}</h4>
+            <h4>
+              {{ data.collage }}
+            </h4>
+            <h5>
+              {{ data.percentageInfo.type }}
+              <span>{{ data.percentageInfo.value }}</span>
+            </h5>
+          </div>
+        </div>
+        <div class="col-xl-6 p-3">
+          <h2>Past Experience</h2>
+        </div>
       </div>
     </div>
   </div>
@@ -18,6 +53,7 @@
 
 <script>
 import Navigation from "../components/Navigation.vue";
+import JSON from "../jsonData/profile.json";
 export default {
   components: { Navigation },
   name: "Resume",
