0f009813d7276d239aa28b1a79b23a27066f7f35
pruthvi7384 <pruthvirajrajput575@gmail.com>
2022-11-28 23:18:28 +0530

    project component completed

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -8,9 +8,36 @@
         <h1>PROJECT</h1>
         <p>My Work</p>
       </div>
-      <div class="row">
-        <div class="col-xl-4"></div>
-        <div class="col-xl-8"></div>
+      <div class="row mt-3 justify-content-center">
+        <div
+          class="col-xl-4 p-3"
+          v-for="(data, index) in JSON.Projects"
+          :key="index"
+        >
+          <div class="project_card">
+            <img :src="data.image" alt="" />
+            <div class="project_info">
+              <h3>{{ data.title }}</h3>
+              <ul>
+                <li v-for="(tech, ind) in data.technology" :key="ind">
+                  {{ tech }}
+                </li>
+              </ul>
+              <div class="link d-flex justify-content-center">
+                <a :href="data.githubLink" target="__blank"
+                  ><button class="btn">
+                    GitHub
+                    <span><i class="fab fa-github"></i></span></button
+                ></a>
+                <a :href="data.liveDemoLink" target="__blank"
+                  ><button class="btn">
+                    Live Preview
+                    <span><i class="fad fa-external-link"></i></span></button
+                ></a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -18,6 +45,7 @@
 
 <script>
 import Navigation from "../components/Navigation.vue";
+import JSON from "../jsonData/profile.json";
 export default {
   components: { Navigation },
   name: "Projects",
