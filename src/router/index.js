22d324edb1087712117688c11892dfbb2619ad47
pruthvi7384 <pruthvirajrajput575@gmail.com>
2022-12-03 22:45:31 +0530

    Route Related Error Solve

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/Home.vue";
 import AboutView from "../views/About.vue";
 import ResumeView from "../views/Resume.vue";
@@ -7,7 +7,7 @@ import ContactView from "../views/Contact.vue";
 import ErrorView from "../views/Error.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
